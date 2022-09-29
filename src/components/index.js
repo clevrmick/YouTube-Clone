57845aaedde91a54459d04f3c083bd0acc6055a6
unknown <clevrmick@gmail.com>
2022-09-29 23:01:20 +0100

    Added the folder structures

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as ChannelDetail } from './ChannelDetail/ChannelDetail';
+export { default as Feed } from './Feed/Feed';
+export { default as SearchFeed } from './SearchFeed/SearchFeed';
+export { default as VideoDetail } from './VideoDetail/VideoDetails';
